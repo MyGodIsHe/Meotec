4bc8aa0ed19f23e9b4f79b779240d6a9d91441fc
Chistyakov Ilya <ilchistyakov@gmail.com>
2011-12-15 12:48:54 +0400

    Set default options

--- meotec/__init__.py
+++ meotec/__init__.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from django.conf import settings
 import global_settings
@@ -13,3 +14,6 @@ for name, value in global_settings.__dict__.items():
 
 if settings.MEOTEC_MANAGERS_ROOT not in sys.path:
     sys.path.append(settings.MEOTEC_MANAGERS_ROOT)
+
+if not os.path.exists(settings.MEOTEC_MANAGERS_ROOT):
+    os.makedirs(settings.MEOTEC_MANAGERS_ROOT)
